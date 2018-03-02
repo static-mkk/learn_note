ed5d9b518641621f3dc07756af364420dc84d4b7
static-mkk <449016106@qq.com>
2018-03-02 21:52:21 +0800

    java DataType

---
+++ DataType.java
@@ -0,0 +1,57 @@
+package cn.kkcoder.map;
+/**
+ * 
+ * @author static-mkk 
+ * @time 2 Mar 2018
+ * @myblog  www.kkcoder.cn
+ *  
+ *
+ */
+
+/**
+ * 
+ *   java的数据类型 （primitive type  8种）
+ *   
+ *   	1.整型（4种）
+ *   		定义：		整型是用于表示没有小数部分的数值类型，有负值。
+ *   		具体类型： 	int(4字节)    short(2字节)    long(8字节)    byte(1字节)
+ *   	
+ *   	2.浮点类型
+ *   		定义：          	用于表示小数部分的数值的类型。
+ *   		具体类型：	float(4字节.有效位数8位)    double(8字节.有效位数15位) 
+ *   		注意：		默认为double类型，大多数情况下使用double类型。   
+ *   					浮点类型使用需要在数值末尾加上f，列入：3.14f  3.15d 。
+ *   					禁止出现在金融行业   需要用BigDecimal类进行处理。
+ *   	
+ *   	3.char类型
+			定义：		表示某个单个字符。
+			具体类型：	char
+			注意：		char a = 'A'; 参见Unicode码 
+ *   	
+ *   	4.boolean类型
+ *   		定义：		用来判断逻辑对错
+ *   		具体类型：	true  false
+ *   		注意：		整型不能喝boolean类型强制转换。
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ *
+ */
+
+
+
+
+
+
+
+
+
+
+
+public class DataType {
+
+}
